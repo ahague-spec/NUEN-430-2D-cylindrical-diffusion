6a696a1a7dd2b3a9f9589766afd0a0cc85c194df
ahague-spec <ahague@tamu.edu>
2025-12-07 15:52:34 -0600

    Update Simulation code.py

--- Simulation code.py
+++ Simulation code.py
@@ -1,7 +1,7 @@
 #start of code, import some stuff
 import numpy as np
 
-#input data that is meant to vary
+#input data that is meant to vary, gonna get it from this: https://www.researchgate.net/figure/BWR-fuel-assembly-dimensions-Fensin-2004-Mueller-et-al-2013a_tbl1_323487844
 Fuel_Radius = 1
 Fuel_ Height = 2
 
@@ -9,6 +9,8 @@ Fuel_ Height = 2
 #This should be our code file, type some stuff in so I know you can access it.
 #Seriously can you see and edit this? 
 #I can see it.
+#Awesome!
 """
+
 
 
