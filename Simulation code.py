871e42e904ef787719c3cf120811c68f4bfa6706
ahague-spec <ahague@tamu.edu>
2025-12-07 15:49:00 -0600

    Update Simulation code.py

--- Simulation code.py
+++ Simulation code.py
@@ -2,10 +2,11 @@
 import numpy as np
 
 #input data that is meant to vary
-Fuel_Rad = 1
+Fuel_Radius = 1
 Fuel_ Height = 2
 
 """
 #This should be our code file, type some stuff in so I know you can access it.
 #Seriously can you see and edit this?
 """
+
