9cfb8a36d3f7a7c2d51427b7c7a0e4aa53a0da29
ahague-spec <ahague@tamu.edu>
2025-12-07 15:46:42 -0600

    Update and rename Old cylindrical lattice code for reference.py to Simulation code.py
    This should get it into a workable state

--- Old cylindrical lattice code for reference.py
+++
@@ -1,6 +0,0 @@
-#start of code, import some stuff
-import numpy as np
-#This should be our code file, type some stuff in so I know you can access it.
-#input data that is meant to vary
-Fuel_Rad = 1
-Fuel_ Height = 2
