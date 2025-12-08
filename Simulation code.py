fa58f7d22242871869451a14253d8cb9686b0532
ahague-spec <ahague@tamu.edu>
2025-12-08 16:53:30 -0600

    Update Simulation code.py
    Added data for fuel/Clad and coolant. Properties that will remain constant.

--- Simulation code.py
+++ Simulation code.py
@@ -2,20 +2,51 @@
 import numpy as np
 
 #input data Rod and clad dimensions from: https://www.researchgate.net/figure/BWR-fuel-assembly-dimensions-Fensin-2004-Mueller-et-al-2013a_tbl1_323487844
-Cylinder_Radius = 5.13e-3  #In meters, done this way to result in lower numbers to avoid potential overflows. This represents fuel + gap + clad radius
-Fuel_ Height = 3.81 #In meters
-#Everything past the cladding is assumed to be coolant
-235_Abs_CS= "PLaceholder" #Absorption cross section of 235 isotope UO2. PLaceholder for now
-238_Abs_CS= "Placeholder" #Asked the professor for reference material, he might be able to direct us to something.
-#Will be some matrices here for scattering cross section
+#All units are at cm-level
+Cylinder_Radius = 0.513 
+Fuel_ Height = 381
+#Everything past the cladding is assumed to be coolant, fuel and clad are homogenized
+#Properties for fuel/clad and moderator. These will remain constant. 
+#No fission in the coolant (duh)
+#Diffusion coefficients are derived from transport cross section (1/(3*(Tr_CS + Abs_CS)))
+  #Fuel/Clad data
+Fuel_Clad_Scat_Matr=[[1.27537E-01, 4.23780E-02, 9.43740E-06, 5.51630E-09, 0.00000E+00, 0.00000E+00, 0.00000E+00],
+[0.00000E+00, 3.24456E-01, 1.63140E-03, 3.14270E-09, 0.00000E+00, 0.00000E+00, 0.00000E+00],
+[0.00000E+00, 0.00000E+00, 4.50940E-01, 2.67920E-03, 0.00000E+00, 0.00000E+00, 0.00000E+00],
+[0.00000E+00, 0.00000E+00, 0.00000E+00, 4.52565E-01, 5.56640E-03, 0.00000E+00, 0.00000E+00],
+[0.00000E+00, 0.00000E+00, 0.00000E+00, 1.25250E-04, 2.71401E-01, 1.02550E-02, 1.00210E-08],
+[0.00000E+00, 0.00000E+00, 0.00000E+00, 0.00000E+00, 1.29680E-03, 2.65802E-01, 1.68090E-02],
+[0.00000E+00, 0.00000E+00, 0.00000E+00, 0.00000E+00, 0.00000E+00, 8.54580E-03, 2.73080E-01]]
 
-#Also gonna get data for fission cross sections, 
+Fuel_Clad_Fission_Spectrum=[0.5879,0.4118,3.391e-4,1.176e-7,0,0,0]
+Fuel_Clad_Abs_CS=[8.025e-3,3.717e-3,2.677e-2,9.624e-2,3.002e-2,0.1113,0.2828]
+Fuel_Clad_Total_CS=[2.12450E-01,3.55470E-01,4.85540E-01,5.59400E-01,3.18030E-01,4.01460E-01,5.70610E-01 ]
+Fuel_Clad_Fission_CS=[7.212e-3,8.193e-4,6.453e-3,1.8565e-2,1.781e-2,8.303e-2,0.216]
+Fuel_Clad_Diff_Coeffs=[1.7924,0.9994,0.6573,0.5123,0.9752,0.6582,0.3935]
+
+  #Moderator data
+Mod_Scat_Matr = [[4.44777E-02, 1.13400E-01, 7.23470E-04, 3.74990E-06, 5.31840E-08, 0.00000E+00, 0.00000E+00],
+[0.00000E+00, 2.82334E-01, 1.29940E-01, 6.23400E-04, 4.80020E-05, 7.44860E-06, 1.04550E-06],
+[0.00000E+00, 0.00000E+00, 3.45256E-01, 2.24570E-01, 1.69990E-02, 2.64430E-03, 5.03440E-04],
+[0.00000E+00, 0.00000E+00, 0.00000E+00, 9.10284E-02, 4.15510E-01, 6.37320E-02, 1.21390E-02],
+[0.00000E+00, 0.00000E+00, 0.00000E+00, 7.14370E-05, 1.39138E-01, 5.11820E-01, 6.12290E-02],
+[00.00000E+00, 0.00000E+00, 0.00000E+00, 0.00000E+00, 0.00000E+00, 1.32440E-01, 2.48070E+00]]
+
+Mod_Diff_Coeffs =[2.0858,0.8071,0.5644,0.5686,0.4606,0.2627,0.1240] #units are cm^-2
+Mod_Abs_CS= [6.0105e-4,1.5793e-5,3.37160E-04,1.94060E-03,5.74160E-03,1.50010E-02,3.72390E-02]
+Mod_Total_CS= [2.30070E-01,7.76460E-01,1.4842,1.5052,1.5592,2.0254,3.3057]
+
+#Scattering is from column group to row group 
+#(i.e 0.2823 is  group 2 -> group 2, 0.1324 is 7->6 etc.)
+#Coordinates in calls are [columns down][rows down] from top left (Which is 0,0)
+
 """
 It might actually be a good idea to create code in a spyder file, then paste it here.
 Data from this source: https://www.oecd-nea.org/science/wprs/eg3drtb/NEA-C5G7MOX.PDF
 There are 7 total energy groups, with tables of data provided for each of them that I will port over Monday evening
 Also, this data homogenizes the fuel and clad
 """
+
 
 
 
