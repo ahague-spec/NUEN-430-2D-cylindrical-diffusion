643839ffac6564c0acf588ba2e4c8a3476eb11fe
ahague-spec <ahague@tamu.edu>
2025-12-11 00:16:35 -0600

    Update Simulation code.py

--- Simulation code.py
+++ Simulation code.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 #input data Rod and clad dimensions from: https://www.researchgate.net/figure/BWR-fuel-assembly-dimensions-Fensin-2004-Mueller-et-al-2013a_tbl1_323487844
 #All units are at cm-level
 One_Group_Toggle=True #Toggle whether or not we're using one group or seven, for analytical testing
-Mesh_Points=100 #How many mesh points are there going to be?
 if One_Group_Toggle==True: #Set number of groups. Code could theoreticall accomodate any number, but we're only using 1 and 7
     Num_Groups= 1
 else:
     Num_Groups = 7
     
 Cylinder_Radius = 0.513 #Fuel and Clad are homogenized, this is in cm
+Mesh_Points=100 #How many mesh points are there going to be in our first trial?
 """Fuel_Height = 381 #Height of overall rod, doesn't matter due to 1D assumption
+#. This is handled at the function for initialization of arrays
 #Everything past the cladding is assumed to be coolant, fuel and clad are homogenized
 #Properties for fuel/clad and moderator. Acquired from this source: https://www.oecd-nea.org/science/wprs/eg3drtb/NEA-C5G7MOX.PDF"""
 
@@ -40,7 +41,7 @@ Mod_Scat_Matr = [[4.44777E-02, 1.13400E-01, 7.23470E-04, 3.74990E-06, 5.31840E-0
 [0.00000E+00, 0.00000E+00, 0.00000E+00, 7.14370E-05, 1.39138E-01, 5.11820E-01, 6.12290E-02],
 [00.00000E+00, 0.00000E+00, 0.00000E+00, 0.00000E+00, 0.00000E+00, 1.32440E-01, 2.48070E+00]]
     #No fission in the moderator (duh)
-Mod_Diff_Coeffs =[2.0858,0.8071,0.5644,0.5686,0.4606,0.2627,0.1240] #units are cm^-2
+Mod_Diff_Coeffs =[2.0858,0.8071,0.5644,0.5686,0.4606,0.2627,0.1240] 
 Mod_Abs_CS= [6.0105e-4,1.5793e-5,3.37160E-04,1.94060E-03,5.74160E-03,1.50010E-02,3.72390E-02]
 Mod_Total_CS= [2.30070E-01,7.76460E-01,1.4842,1.5052,1.5592,2.0254,3.3057]
 
@@ -48,33 +49,45 @@ Mod_Total_CS= [2.30070E-01,7.76460E-01,1.4842,1.5052,1.5592,2.0254,3.3057]
 #(i.e 0.2823 is  group 2 -> group 2, 0.1324 is 7->6 etc.)
 #Coordinates in calls are [rows down][columns right] from top left (Which is 0,0)
 This is just reference material for me, cuz I tend to forget python does x,y in the opposite way it usually is"""
+#def Initialize(Mesh_Points): #Set all of the empty/zeroes arrays, in case multiple trials need to be run. Might be a pain because global variables
 
 Greatest_Extents=np.empty(Num_Groups) #based on zero-edge assumptuion, find the zero-point for each energy group
-for i in range(Num_Groups): #Loop populates extrapolation distance array. Contains positions for zero-flux assumption
+for i in range(Num_Groups): #Loop populates extrapolation distance array. Contains positions for zero-flux assumption. Constant
     Greatest_Extents[i]=Cylinder_Radius+2*(Mod_Diff_Coeffs[i]) 
-    
 X_Points=np.linspace(0,max(Greatest_Extents),Mesh_Points) #10 points between centerline and greatest extent. Not all groups will reach this far
 Flux_Array=np.zeros((Num_Groups,len(X_Points)))#Create empty array to store flux values at each point
+Anal_Flux=np.zeros((len(X_Points))) #Create 1D array to store flux values for 1-group analytical solution
 #Loop just defines values for graph testing,
-for i in range(Num_Groups): #Run this loop
+for i in range(Num_Groups): #Run this loop to define placeholder values for Flux_Array
     for j in range(Mesh_Points):
         if X_Points[j]>Greatest_Extents[i]:
             Flux_Array[i][j]=0
+            Anal_Flux[j]=0
         else:
             Flux_Array[i][j]=i**2 + j**2
-        
-#Print out flux values for each group
-plt.plot(X_Points,Flux_Array[0],color="red",label="Group 1 Flux")
-if (One_Group_Toggle == False): #Only try printing out other groups if multi-group assumption happens
-    plt.plot(X_Points,Flux_Array[1],color="orange",label="Group 2 Flux")
-    plt.plot(X_Points,Flux_Array[2],color="yellow",label="Group 3 Flux")
-    plt.plot(X_Points,Flux_Array[3],color="green",label="Group 4 Flux")
-    plt.plot(X_Points,Flux_Array[4],color="blue",label="Group 5 Flux")
-    plt.plot(X_Points,Flux_Array[5],color="indigo",label="Group 6 Flux")
-    plt.plot(X_Points,Flux_Array[6],color="violet",label="Group 7 Flux")
-plt.axvline(x=Cylinder_Radius, color='black',linestyle='--', label='Edge of Fuel/Clad') #Indicate edge of the cylinder
-plt.xlabel("Radial distance from center (cm)")
-plt.ylabel("Group Flux (n / (cm\u00b2 * s)")
-plt.legend()
-plt.show()
+
+def Find_L2_Error(): #For analytical solution, finds Least mean squared error between it and numerical solution
+    print("gaming")
+    #Transfer this to spyder
+
+def Plot_Fluxes(): #Print out flux values for each group
+    if (One_Group_Toggle == False): #Print out all 7 groups in pretty rainbow colors
+        plt.plot(X_Points,Flux_Array[0],color="red",label="Group 1 Flux")
+        plt.plot(X_Points,Flux_Array[1],color="orange",label="Group 2 Flux")
+        plt.plot(X_Points,Flux_Array[2],color="yellow",label="Group 3 Flux")
+        plt.plot(X_Points,Flux_Array[3],color="green",label="Group 4 Flux")
+        plt.plot(X_Points,Flux_Array[4],color="blue",label="Group 5 Flux")
+        plt.plot(X_Points,Flux_Array[5],color="indigo",label="Group 6 Flux")
+        plt.plot(X_Points,Flux_Array[6],color="violet",label="Group 7 Flux")
+    else: #If only one group, we only print out the first group and compare it to the analytical solution
+        plt.plot(X_Points,Flux_Array[0],color="red",label="Numerical Flux")
+        plt.plot(X_Points,Anal_Flux,color="blue",linestyle='-',label='Analytical Flux')
+    plt.axvline(x=Cylinder_Radius, color='black',linestyle='--', label='Edge of Fuel/Clad') #Indicate edge of the cylinder
+    plt.xlabel("Radial distance from center (cm)")
+    plt.ylabel("Group Flux (n / (cm\u00b2 * s)")
+    plt.legend()
+    plt.show()
+Plot_Fluxes() #Run plotting code
 #End of code
+
+
